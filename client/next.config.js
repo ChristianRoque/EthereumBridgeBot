d33c643fd2bd6aa8ccc6b5be160c457f3753d386
ChristianRoque <croquevaldes@hotmail.com>
2021-07-15 22:38:03 -0400

    deployment

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
-  target: "serverless",
   images: {
     loader: "imgix",
     path: "",
--- client/pages/index.js
+++ client/pages/index.js
@@ -3,5 +3,9 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div className={styles.container}></div>;
+  return (
+    <div>
+      <div>Hello world</div>
+    </div>
+  );
 }
