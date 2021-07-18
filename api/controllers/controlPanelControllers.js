26c7ae9f9862926449c7af1bfd559d89c703e9e2
ChristianRoque <croquevaldes@hotmail.com>
2021-07-18 02:08:52 -0400

    sign and send transactions functioning

--- api/controllers/controlPanelControllers.js
+++ api/controllers/controlPanelControllers.js
@@ -1,49 +1,59 @@
 const Web3 = require("web3");
 const path = require("path");
-const ethereum = require("@ethereumjs/tx");
-const Transaction = ethereum.Transaction;
+const Tx = require("@ethereumjs/tx");
 const contractJSON = require("../abis/Bank.json");
-require("dotenv").config({ path: path.join(__dirname, "../../.env") });
+const Common = require("@ethereumjs/common").default;
 
+require("dotenv").config({ path: path.join(__dirname, "../../.env") });
+// Create web3 instance connected to node provider
 let web3 = new Web3(
+  // In this case we will use infura
+  // Refer to web3 docs for more customization
   new Web3.providers.WebsocketProvider(
     "wss://mainnet.infura.io/ws/v3/" + process.env.INFURA_KEY
   )
 );
 
+// Import user and contract data
 const account = process.env.ADDRESS;
 const privateKey = Buffer.from(process.env.PRIVATE_KEY, "hex");
+// Create contract instance
 const contractAddress = process.env.CONTRACT_ADDRESS;
 const contract = new web3.eth.Contract(contractJSON.abi, contractAddress);
 
 exports.fundContract = function (req, res) {
-  let myData = contract.methods.getContractBalance().encodeABI();
+  // Sign and send transactions to ethereum chain
+  // Read docs for more customization
+  // https://web3js.readthedocs.io/en/v1.3.4/web3-eth.html
 
+  let myData = contract.methods.anyMethod().encodeABI();
   web3.eth.getTransactionCount(account, (err, txCount) => {
     const txObject = {
       nonce: web3.utils.toHex(txCount),
-      to: contractAddress,
-      value: web3.utils.toHex(web3.utils.toWei("0", "ether")),
+      from: account,
+      to: contract,
+      value: web3.utils.toHex(web3.utils.toWei("0.002", "ether")),
       gasLimit: web3.utils.toHex(21000),
-      gasPrice: web3.utils.toHex(web3.utils.toWei("10", "gwei")),
-      data: myData,
+      gasPrice: web3.utils.toHex(web3.utils.toWei("20", "gwei")),
     };
 
     const common = new Common({ chain: "mainnet" });
-    const tx = Transaction.fromTxData(txObject, { common });
+    const tx = Tx.Transaction.fromTxData(txObject, { common });
 
-    tx.sign(privateKey);
+    const signedTx = tx.sign(privateKey);
+    const serializedTx = signedTx.serialize();
 
-    const serializedTx = tx.serialize();
-    const raw = "0x" + serializedTx.toString("hex");
-
-    web3.eth.sendSignedTransaction(raw, (err, txHash) => {
-      console.log("txHash:", txHash);
-    });
+    let txHash = web3.eth
+      .sendSignedTransaction(web3.utils.toHex(serializedTx))
+      .on("receipt", console.log)
+      .catch((e) => console.log("e: ", e));
   });
 };
 
 exports.getContractBalance = async function (req, res) {
+  // Pull data from contract using web3 and infura node
+  // Read docs for more customization
+  // https://web3js.readthedocs.io/en/v1.3.4/web3-eth-contract.html
   try {
     const response = await contract.methods.getContractBalance().call();
     console.log(response);
--- api/routes/controlRoutes.js
+++ api/routes/controlRoutes.js
@@ -2,4 +2,5 @@ module.exports = function (app) {
   var contract = require("../controllers/controlPanelControllers");
 
   app.route("/balance").get(contract.getContractBalance);
+  app.route("/fund").get(contract.fundContract);
 };
