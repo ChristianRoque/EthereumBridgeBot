e4cacd1c6faa52178aee2934a2aa0a7f394c3ebb
ChristianRoque <croquevaldes@hotmail.com>
2021-07-20 00:25:34 -0400

    created new instance of web3 using a bsc node provider, created function to interact with bsc contract, created a route for bsc contract

--- api/controllers/controlPanelControllers.js
+++ api/controllers/controlPanelControllers.js
@@ -3,11 +3,13 @@ const path = require("path");
 const Transaction = require("@ethereumjs/tx").Transaction;
 const contractJSON = require("../abis/Bank.json");
 const Common = require("@ethereumjs/common").default;
+const Moralis = require("moralis/node");
 
 require("dotenv").config({ path: path.join(__dirname, "../../.env") });
+
 // Create web3 instance connected to node provider
 let web3ETH = new Web3(
-  // In this case we will use infura
+  // In this case we will use infura to ethereum mainnet chain
   // Refer to web3 docs for more customization
   new Web3.providers.WebsocketProvider(
     "wss://mainnet.infura.io/ws/v3/" + process.env.INFURA_KEY
@@ -15,8 +17,12 @@ let web3ETH = new Web3(
 );
 
 let web3BNB = new Web3(
+  // In this case we will use moralis connecto to BSC chain
+  // Refer to web3 docs for more customization
   new Web3.providers.WebsocketProvider(
-    "wss://mainnet.infura.io/ws/v3/" + process.env.INFURA_KEY
+    "wss://speedy-nodes-nyc.moralis.io/" +
+      process.env.MORALIS_KEY +
+      "/bsc/mainnet/ws"
   )
 );
 
@@ -34,14 +40,14 @@ exports.ethereumContract = async function (req, res) {
 
   // let myData = contract.methods.anyMethod().encodeABI();
 
-  web3.eth.getTransactionCount(account, (err, txCount) => {
+  web3ETH.eth.getTransactionCount(account, (err, txCount) => {
     const txObject = {
-      nonce: web3.utils.toHex(txCount),
+      nonce: web3ETH.utils.toHex(txCount),
       from: account,
       to: contract,
-      value: web3.utils.toHex(web3.utils.toWei("0.001", "ether")),
-      gasLimit: web3.utils.toHex(21000),
-      gasPrice: web3.utils.toHex(web3.utils.toWei("20", "gwei")),
+      value: web3ETH.utils.toHex(web3ETH.utils.toWei("0.001", "ether")),
+      gasLimit: web3ETH.utils.toHex(21000),
+      gasPrice: web3ETH.utils.toHex(web3ETH.utils.toWei("20", "gwei")),
       // data: myData,
     };
 
@@ -51,19 +57,50 @@ exports.ethereumContract = async function (req, res) {
     const signedTx = tx.sign(privateKey);
     const serializedTx = signedTx.serialize();
 
-    let txHash = web3.eth
-      .sendSignedTransaction(web3.utils.toHex(serializedTx))
+    let txHash = web3ETH.eth
+      .sendSignedTransaction(web3ETH.utils.toHex(serializedTx))
       .on("receipt", console.log)
       .catch((e) => console.log("e: ", e));
   });
 };
 
 exports.binanceContract = async function (req, res) {
-  const common = Common.custom({
-    name: "bnb",
-    chainId: 56,
-    networkId: 56,
-    defaultHardfork: "'petersburg'",
+  // Sign and send transactions to ethereum chain
+  // Read docs for more customization
+  // https://web3js.readthedocs.io/en/v1.3.4/web3-eth.html
+
+  let response = await web3BNB.eth.getBlockNumber();
+  console.log(response);
+
+  // let myData = contract.methods.anyMethod().encodeABI();
+
+  web3BNB.eth.getTransactionCount(account, (err, txCount) => {
+    const txObject = {
+      nonce: web3BNB.utils.toHex(txCount),
+      from: account,
+      to: contract,
+      value: web3BNB.utils.toHex(web3BNB.utils.toWei("0.001", "ether")),
+      gasLimit: web3BNB.utils.toHex(21000),
+      gasPrice: web3BNB.utils.toHex(web3BNB.utils.toWei("20", "gwei")),
+      // data: myData,
+    };
+
+    const common = Common.custom({
+      name: "bnb",
+      chainId: 56,
+      networkId: 56,
+      defaultHardfork: "'petersburg'",
+    });
+
+    const tx = Transaction.fromTxData(txObject, { common });
+
+    const signedTx = tx.sign(privateKey);
+    const serializedTx = signedTx.serialize();
+
+    let txHash = web3BNB.eth
+      .sendSignedTransaction(web3BNB.utils.toHex(serializedTx))
+      .on("receipt", console.log)
+      .catch((e) => console.log("e: ", e));
   });
 };
 
--- api/routes/controlRoutes.js
+++ api/routes/controlRoutes.js
@@ -2,4 +2,5 @@ module.exports = function (app) {
   var contract = require("../controllers/controlPanelControllers");
 
   app.route("/firstContract").get(contract.ethereumContract);
+  app.route("/secondContract").get(contract.binanceContract);
 };
