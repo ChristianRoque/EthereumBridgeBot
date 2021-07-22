94f9a95cc01efa38ef91c413ff78c0f069ea2efc
ChristianRoque <croquevaldes@hotmail.com>
2021-07-22 02:02:46 -0400

    dsuccessful deploy using heroku

--- client/pages/api/contract-data.js
+++ client/pages/api/contract-data.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://reverent-mcnulty-a240c9.netlify.app/",
+  baseURL: "https://vast-atoll-52124.herokuapp.com/",
 });
 
 const errHandler = (err) => {
